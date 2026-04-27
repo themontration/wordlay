d6c60c18aaba6ccbcb3db852e1b202589b156b0e
themontration <themontration@gmail.com>
2026-04-27 20:20:15 +0330

    Update index.js

--- api/index.js
+++ api/index.js
@@ -2,26 +2,8 @@ export default async function handler(req, res) {
   const TARGET_BASE = (process.env.TARGET_DOMAIN || "").replace(/\/$/, "");
   
   if (!TARGET_BASE) {
-    return res.status(500).send("Misconfigured: TARGET_DOMAIN is not set");
+    return res.status(500).send("Misconfigured");
   }
 
-  try {
-    const targetUrl = TARGET_BASE + req.url;
-    
-    const response = await fetch(targetUrl, {
-      method: req.method,
-      headers: req.headers,
-      body: req.method !== "GET" && req.method !== "HEAD" ? req : undefined,
-      redirect: "manual",
-    });
-
-    res.status(response.status);
-    response.headers.forEach((v, k) => res.setHeader(k, v));
-    const body = await response.arrayBuffer();
-    res.end(Buffer.from(body));
-    
-  } catch (err) {
-    console.error("relay error:", err);
-    res.status(502).send("Bad Gateway");
-  }
+  res.status(200).send("relay works: " + req.url);
 }
